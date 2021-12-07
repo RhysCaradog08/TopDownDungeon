d3f1268efb76ea14acec33b4bc5ca9b738131942
RhysCaradog08 <55874291+RhysCaradog08@users.noreply.github.com>
2021-12-07 16:18:35 +0000

    BarbarianController.cs and RogueController.cs
    8 directional movement copied from KnightController.cs to BarbarianController.cs and RogueController.cs

---
+++ TopDownDungeon/Assets/Scripts/RogueController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RogueController : MonoBehaviour
+{
+    CharacterController cc;
+
+    public float moveSpeed;
+    public float turnSpeed;
+
+    Vector2 input;
+    float angle;
+
+    Quaternion targetRotation;
+    Transform cam;
+
+    private void Start()
+    {
+        cc = GetComponent<CharacterController>();
+
+        cam = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        GetInput();
+
+        if (Mathf.Abs(input.x) < 1 && Mathf.Abs(input.y) < 1) return;
+
+        CalculateDirection();
+        RotatePlayer();
+        MovePlayer();
+    }
+
+    void GetInput()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void CalculateDirection()
+    {
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+        angle += cam.eulerAngles.y;
+    }
+
+    void RotatePlayer()
+    {
+        targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+    }
+
+    void MovePlayer()
+    {
+        cc.Move(transform.forward * moveSpeed * Time.deltaTime);
+    }
+}
